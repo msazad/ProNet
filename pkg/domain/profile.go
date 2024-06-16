b24e4eef89c43dc27d5cb5cbf5c60119966e0d83
user <msazad202@gmail.com>
2024-06-16 23:26:55 +0530

    modified profile in domain

--- pkg/domain/profile.go
+++ pkg/domain/profile.go
@@ -1,13 +1,15 @@
 package domain
 
+import "gorm.io/gorm"
+
 type Profile struct {
-	ID                uint `json:"id" gorm:"primarykey"`
-	ApplicantID       uint
-	ResumeFileAddress string
-	Skills            string
-	Education         string
-	Experience        string
-	Name              string
-	Email             string
-	Phone             string
+	gorm.Model
+	UserID            uint   `json:"user"`
+	ResumeFileAddress string `json:"resume_file_address" gorm:"not null"`
+	Skills            string `json:"skills" gorm:"not null"`
+	Education         string `json:"education" gorm:"not null"`
+	Experience        string `json:"experience" gorm:"not null"`
+	Name              string `json:"name" gorm:"validation required"`
+	Email             string `json:"email" gorm:"not null"`
+	Phone             string `json:"phone" gorm:"not null"`
 }
