747219383d611287874651ae8f8e1a0e060c106d
user <msazad202@gmail.com>
2024-06-18 23:14:58 +0530

    added login handler in usecase

---
+++ pkg/usecase/admin.go
@@ -0,0 +1,51 @@
+package usecase
+
+import (
+	"fmt"
+	"linkdin/pkg/domain"
+	"linkdin/pkg/repository/interfaces"
+	services "linkdin/pkg/usecase/interfaces"
+	"linkdin/pkg/utils/models"
+
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type adminUsecase struct {
+	adminRepository interfaces.AdminRepository
+}
+
+func NewAdminUsecase(adRepo interfaces.AdminRepository) services.AdminUsecase {
+	return &adminUsecase{
+		adminRepository: adRepo,
+	}
+}
+
+func (au *adminUsecase) LoginHandler(adminDetails models.AdminLogin) (domain.AdminToken, error) {
+	//Getting details of the admin based on the email provided
+	adminCompareDetails, _ := au.adminRepository.LoginHandler(adminDetails)
+
+	if adminCompareDetails.Email != adminDetails.Email {
+		return domain.AdminToken{}, errors.New("admin not exist")
+
+	}
+	//Compare password from database that provided by admin
+	err := bcrypt.CompareHashAndPassword([]byte(adminCompareDetails.PasswordHash), []byte(adminDetails.Password))
+	if err != nil {
+		return domain.AdminToken{}, err
+	}
+	var adminDetailsResponse models.AdminDetailsResponse
+	adminDetailsResponse.ID = int(adminCompareDetails.ID)
+	adminDetailsResponse.Name=adminCompareDetails.Name
+	adminDetailsResponse.Email=adminCompareDetails.Email
+	fmt.Println("reached the admindetailsresponse")
+	token,refresh,err:=helper.GenerateAdminToken(adminDetailsResponse)
+	if err!=nil{
+		return domain.AdminToken{},err
+	}
+	return domain.AdminToken{
+		Admin: adminDetailsResponse,
+		Token: token,
+		RefreshToken: refresh,
+	},nil
+}
--- pkg/utils/models/admin.go
+++ pkg/utils/models/admin.go
@@ -1,12 +1,16 @@
 package models
 
-
-type AdminLogin struct{
-	Email string `json:"email,omitempty" validate:"required"`
+type AdminLogin struct {
+	Email    string `json:"email,omitempty" validate:"required"`
 	Password string `json:"password" validate:"min=6,max=12"`
 }
-type AdminDetailsResponse struct{
-	ID int `json:"id"`
-	Name string `json:"name"`
+type AdminDetailsResponse struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
 	Email string `json:"email"`
+}
+type AdminToken struct {
+	Admin        AdminDetailsResponse
+	Token        string
+	RefreshToken string
 }
